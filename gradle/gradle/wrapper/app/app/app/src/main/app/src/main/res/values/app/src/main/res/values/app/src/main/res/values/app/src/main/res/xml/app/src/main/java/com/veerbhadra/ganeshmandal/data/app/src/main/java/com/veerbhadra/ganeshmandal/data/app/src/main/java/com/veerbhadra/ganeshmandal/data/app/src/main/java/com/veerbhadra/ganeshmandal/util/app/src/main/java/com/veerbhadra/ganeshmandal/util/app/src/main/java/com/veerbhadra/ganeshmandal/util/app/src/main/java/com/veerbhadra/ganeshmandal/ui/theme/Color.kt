fc28c304413873b8c9bc093d739f22131d1fcd39
Atrey1109 <upareatrey@gmail.com>
2026-08-21 03:11:08 +0530

    Create Color.kt

---
+++ gradle/gradle/wrapper/app/app/app/src/main/app/src/main/res/values/app/src/main/res/values/app/src/main/res/values/app/src/main/res/xml/app/src/main/java/com/veerbhadra/ganeshmandal/data/app/src/main/java/com/veerbhadra/ganeshmandal/data/app/src/main/java/com/veerbhadra/ganeshmandal/data/app/src/main/java/com/veerbhadra/ganeshmandal/util/app/src/main/java/com/veerbhadra/ganeshmandal/util/app/src/main/java/com/veerbhadra/ganeshmandal/util/app/src/main/java/com/veerbhadra/ganeshmandal/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.veerbhadra.ganeshmandal.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MaroonDark = Color(0xFF4A0E17)
+val MaroonPrimary = Color(0xFF721C24)
+val MaroonLight = Color(0xFF9E2A36)
+val AntiqueGold = Color(0xFFC59B27)
+val GoldLight = Color(0xFFE5C158)
+val IvoryBackground = Color(0xFFFDFBF7)
+val IvorySurface = Color(0xFFFFFDF9)
+val TextDark = Color(0xFF261114)
+val TextMuted = Color(0xFF6B585A)
